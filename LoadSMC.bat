f19c64eed0ecdbbff442e5ea5607e8b9079cb19f
Steve Ives <steve.ives@synergex.com>
2020-05-28 15:24:03 -0700

    Cleaned up environment

--- LoadSMC.bat
+++ LoadSMC.bat
@@ -6,25 +6,25 @@ rem Configure a Synergy environment
 rem call "%SYNERGYDE32%dbl\dblvars32.bat"
 call "%SYNERGYDE64%dbl\dblvars64.bat"
 
-set XFPL_SMCPATH=.
+set XFPL_SMCPATH=.\SMC\
 
 rem Delete any existing file to make sure we get a new one
-if exist smc.xml del /q smc.xml
+if exist "%XFPL_SMCPATH%smc.xml" del /q "%XFPL_SMCPATH%smc.xml"
 
 rem Generate a new SMC XML file from our method sources
 echo Processing source files...
-dbl2xml *.dbl -out smc.xml
+dbl2xml .\xfplMethods\*.dbl -out "%XFPL_SMCPATH%smc.xml"
 if ERRORLEVEL 1 goto parse_fail
 
 rem Load the XML file into the SMC
 echo Loading method catalog...
-dbs DBLDIR:mdu -u smc.xml
+dbs DBLDIR:mdu -u "%XFPL_SMCPATH%smc.xml"
 if ERRORLEVEL 1 goto load_fail
 echo Method catalog was loaded
 
 rem Unload the SMCback to an XML file
 echo Unloading method catalog...
-dbs DBLDIR:mdu -e smc.xml
+dbs DBLDIR:mdu -e "%XFPL_SMCPATH%smc.xml"
 if ERRORLEVEL 1 goto load_fail
 
 goto done
